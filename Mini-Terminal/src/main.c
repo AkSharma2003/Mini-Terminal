eeba6a6e87cf9aa1135da7d32bc862eed2941b6a
Ankit Kumar Sharma <ankit824012@gmail.com>
2025-11-01 19:46:32 +0530

    add new feature for ls and pwd and look likre real terminal

--- Mini-Terminal/src/main.c
+++ Mini-Terminal/src/main.c
@@ -2,16 +2,27 @@
 #include <string.h> 
 #include <stdlib.h> 
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
 
 #define MAX_INPUT 1024
-
+#define MAX_ARG 64
 int main(){
     char input[MAX_INPUT];
     char *input_result; // To catch the return value of fgets
+    char *args[MAX_ARG];
 
     while (1)
     {    
-        printf("KAR_T>");
+        char cwd[MAX_INPUT];
+
+        if(getcwd(cwd, sizeof(cwd)) != NULL){
+            printf("%s KAR_T> ", cwd);
+        } else {
+            perror("getcwd failed");
+            printf("KAR_T> ");
+        }
         fflush(stdout); // Ensures prompt displays immediately
         
         // Use input_result to store the return value of fgets
@@ -39,11 +50,48 @@ int main(){
         }
         
         // 4. ECHO the input (for debugging)
-        if (strlen(input) > 0) {
-            printf("You entered: [%s]\n", input);
+       
+        // 5. creating Token
+
+        char *token = strtok(input, " \t\n");
+        int i=0;
+
+        while (token!=NULL && i<MAX_ARG)
+        {
+            args[i++]=token;
+            token=strtok(NULL," \t\n");
         }
 
-        // --- Next step: Tokenization and Execution Logic will go here ---
+        args[i]=NULL;
+
+        if(strcmp(args[0],"cd")==0){
+            if(args[1]==NULL){
+                perror("cd: Missing argument");
+            }
+            else if (chdir(args[1])!=0)
+            {
+                perror("cd failed");
+            }
+            continue;
+        }
+
+        pid_t pid=fork();
+
+        if (pid == 0) {
+            execvp(args[0],args);
+            perror("execvp failed");
+            exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            int status;
+            waitpid(pid,&status,0);
+            // printf("Exist status: %d\n",WEXITSTATUS(status));
+        } else {
+            perror("fork failed");
+        }
+        
+        // if (strlen(input) > 0) {
+        //     printf("You entered: [%s]\n", input);
+        // }
     }
     
     return 0;
